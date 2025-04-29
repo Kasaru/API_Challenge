b3c547204649cb54e93677ac0ca5abc923ce1b1c
Artem <arte.kats@gmail.com>
2025-04-29 11:02:15 +0300

    Update pytest configurations and refine invalid ID range
    Added new negative test cases to pytest.ini for enhanced validation. Adjusted the invalid_todo_id range in endpoints.py to ensure more distinct testing scenarios.

--- endpoints.py
+++ endpoints.py
@@ -4,6 +4,6 @@ challenges = '/challenges'
 todos = '/todos'
 invalid_todo_endpoint = '/todo'
 todo_id = f'/todos/{random.randint(1,10)}'
-invalid_todo_id = f'/todos/{random.randint(11,100)}'
+invalid_todo_id = f'/todos/{random.randint(999,9999)}'
 
 done_status = '?doneStatus=true'
